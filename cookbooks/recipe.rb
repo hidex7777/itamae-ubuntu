4ead8d68b7ba60ea89b82e541d6bf05ba47b152f
hidex7777 <hidex7777@gmail.com>
2015-08-18 13:19:54 +0900

    fix for ubuntu

--- cookbooks/recipe.rb
+++ cookbooks/recipe.rb
@@ -14,12 +14,10 @@ PROFILE = "/home/vagrant/.profile"
 
 git RBENV_DIR do
   repository "git://github.com/sstephenson/rbenv.git"
-  user 'vagrant'
 end
 
 git "#{RBENV_DIR}/plugins/ruby-build" do
   repository "git://github.com/sstephenson/ruby-build.git"
-  user 'vagrant'
 end
 
 remote_file '/home/vagrant/.bashrc' do
@@ -37,18 +35,18 @@ end
 execute "install ruby 2.2.2" do
   command ". #{PROFILE}; rbenv install 2.2.2"
   user 'vagrant'
-  not_if "rbenv versions | grep 2.2.2"
+  not_if ". #{PROFILE}; rbenv versions | grep 2.2.2"
 end
 
 execute "set global ruby" do
   command ". #{PROFILE}; rbenv global 2.2.2; rbenv rehash"
   user 'vagrant'
-  not_if "rbenv global | grep 2.2.2"
+  not_if ". #{PROFILE}; rbenv global | grep 2.2.2"
 end
 
 execute "gem install gems" do
   command ". #{PROFILE}; gem install bundler; rbenv rehash"
   user 'vagrant'
-  not_if "gem list | grep bundler"
+  not_if ". #{PROFILE}; gem list | grep bundler"
 end
 
